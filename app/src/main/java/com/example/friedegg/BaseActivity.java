077087809e3b71736762677aa15feb2bd2ff106d
CaoRunze <1206654797@qq.com>
2016-05-23 18:54:03 +0800

    主界面

---
+++ app/src/main/java/com/example/friedegg/BaseActivity.java
@@ -0,0 +1,40 @@
+package com.example.friedegg;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 123 on 2016/5/23.
+ * BaseActivity
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+    protected Context mContext;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        mContext = this;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Abstract Method In Activity
+    ///////////////////////////////////////////////////////////////////////////
+
+    protected abstract void initView();
+
+    protected abstract void initData();
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Common Operation
+    ///////////////////////////////////////////////////////////////////////////
+
+    public void replaceFragment(int id_content, Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(id_content, fragment);
+        transaction.commit();
+    }
+}
--- app/src/main/java/com/example/friedegg/MainActivity.java
+++ app/src/main/java/com/example/friedegg/MainActivity.java
@@ -1,13 +1,118 @@
 package com.example.friedegg;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.friedegg.modul.NetWorkEvent;
+import com.example.friedegg.utils.NetWorkUtil;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+
+public class MainActivity extends BaseActivity {
+
+    private BroadcastReceiver netStateReceiver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
+    }
+
+    @Override
+    protected void initView() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        assert toolbar != null;
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ActionBarDrawerToggle mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                invalidateOptionsMenu();
+            }
+        };
+        mActionBarDrawerToggle.syncState();
+        assert mDrawerLayout != null;
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+    }
+
+    @Override
+    protected void initData() {
+        netStateReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+                    if (NetWorkUtil.isNetWorkConnected(MainActivity.this)) {
+                        EventBus.getDefault().post(new NetWorkEvent(NetWorkEvent.AVAILABLE));
+                    } else {
+                        EventBus.getDefault().post(new NetWorkEvent(NetWorkEvent.UNAVAILABLE));
+                    }
+                }
+            }
+        };
+
+        registerReceiver(netStateReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Subscribe
+    public void onEvent(NetWorkEvent event) {
+        if (event.getType() == NetWorkEvent.UNAVAILABLE) {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+            alertDialog.setTitle("无法连接网络").setMessage("去开启网络？").setPositiveButton("是", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
+                    startActivity(intent);
+                }
+            }).setNegativeButton("否", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(netStateReceiver);
     }
 }
